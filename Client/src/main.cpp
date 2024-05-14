87e25eed327788cf254260bd4cff96f2eb51d4e8
chansongyou <dbcksthd13@gmail.com>
2024-05-14 23:44:40 +0900

    write tutorial code using tcp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,6 +1,35 @@
+#include <array>
 #include <iostream>
+#include <boost/asio.hpp>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    try
+    {
+        boost::asio::io_context io_context;
+
+        boost::asio::ip::tcp::socket socket(io_context);
+        
+        socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 13));
+
+        while(true){
+            std::array<char, 128> buf;
+            boost::system::error_code error;
+
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+                break;  // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+            
+            std::cout.write(buf.data(), len);
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
 }
--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,6 +1,36 @@
+#include <ctime>
 #include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+std::string make_daytime_string()
+{
+    std::time_t now = time(0);
+    return ctime(&now);
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    try
+    {
+        boost::asio::io_context io_context;
+        boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 13));
+
+        while(true)
+        {
+            boost::asio::ip::tcp::socket socket(io_context);
+            acceptor.accept(socket);
+
+            std::string message = make_daytime_string();
+
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
 }
